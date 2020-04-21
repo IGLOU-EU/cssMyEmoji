623bf4daea73c98ef6755abdf479f39712d9cd74
gil <gino.ladowitch@gmail.com>
2020-04-21 18:02:47 +0200

    Copy to clipboard OK: - added main.js file - copy emoji icon on click - copy class text on class name click

---
+++ main.js
@@ -0,0 +1,23 @@
+const emojis = Array.from(document.getElementsByClassName('emoji'));
+
+const copyToClipBoard = (text) => {
+    navigator.clipboard.writeText(text);
+}
+
+emojis.forEach(emoji => {
+
+    emoji.addEventListener(
+        'click', (event) => {
+        let contentBefore = getComputedStyle(emoji, ':before').getPropertyValue('content').replace('"', '').replace('"', '');
+        copyToClipBoard(contentBefore);
+        }
+    );
+
+    emoji.getElementsByTagName('sup')[0].addEventListener(
+        'click', (event) => {
+            event.stopPropagation();
+            let className = `.${event.target.parentElement.classList[1]}`;
+            copyToClipBoard(className);
+        }
+    );
+});
